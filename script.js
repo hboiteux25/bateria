13ec8ee15c251e1e342b41c8c7b1626d03c57a99
Henrique Boiteux <92280899+hboiteux25@users.noreply.github.com>
2022-05-27 12:06:38 -0300

    commit

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ document.body.addEventListener('keyup', (event)=>{
     playSound(event.code.toLowerCase());
 })
 
+
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
+    
+    if(song !== ''){
+        let songArray = song.split('');
+        playComposition(songArray);
+    }
+})
+
 function playSound(sound){
     let audioElement = document.querySelector(`#s_${sound}`);
     let keyElement = document.querySelector(`div[data-key="${sound}"]`)
 
     if(audioElement){
+        audioElement.currentTime = 0;
         audioElement.play();
     }
 
@@ -16,5 +27,21 @@ function playSound(sound){
         setTimeout(()=>{
             keyElement.classList.remove('active')
         },300);
+    }
+}
+
+
+//Tocar musica no botão e colocar um tempo para executar
+function playComposition(songArray){
+    let wait = 0;
+    for(let songItem of songArray){
+
+        setTimeout(()=>{
+
+            playSound(`key${songItem}`);
+
+        },wait)
+
+        wait +=250;
     }
 }
